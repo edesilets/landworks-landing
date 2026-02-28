933f5fd4f1e8bf83679bef8a8ce3ff94c8ffb7f4
Ethan <erdesi90562@gmail.com>
2026-02-28 16:49:15 -0500

    Use new Logos and content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,29 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from '@tailwindcss/vite'
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: "https://example.com",
+
+  plugins: [
+    tailwindcss(),
+  ],
+
+  // adapter: cloudflare({
+  // 	platformProxy: {
+  // 		enabled: true,
+  // 	},
+  // }),
+  integrations: [mdx(), sitemap()],
+
   server: {
     port: 8787,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
