3df1706d26ee5d73ba4e0a2244fb419b6fab6a70
Ethan <erdesi90562@gmail.com>
2026-02-28 16:52:02 -0500

    Undo astro config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite'
-// import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-
   plugins: [
     tailwindcss(),
   ],
-
-  // adapter: cloudflare({
-  // 	platformProxy: {
-  // 		enabled: true,
-  // 	},
-  // }),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   integrations: [mdx(), sitemap()],
 
   server: {
